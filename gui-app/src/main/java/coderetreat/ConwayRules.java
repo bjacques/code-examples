7edf75df1320fc8f58d39d89d1e8bb1f67e528b9
Brad Jacques <bradleyjacques@hotmail.com>
2014-11-16 23:33:37 +0000

    complete coderetreat tests + impl

--- gui-app/src/main/java/coderetreat/ConwayRules.java
+++ gui-app/src/main/java/coderetreat/ConwayRules.java
@@ -1,5 +1,7 @@
 package coderetreat;
 
+import static coderetreat.PointFactory.createRelativePoint;
+
 import java.awt.Point;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,12 +10,11 @@ import com.google.common.collect.Sets;
 
 public class ConwayRules {
 
-	public Set<Point> getAllCellsToCheck(Set<Point> liveCells) {
+	public Set<Point> getAllLocationsThatCanBeAffected(Set<Point> liveCells) {
 		Set<Point> points = new HashSet<Point>();
 		for(Point cell : liveCells) {
 			points.addAll(Sets.newHashSet(
 			new Point(cell.x, cell.y+1),
-			new Point(cell.x, cell.y),
 			new Point(cell.x, cell.y-1),
 			new Point(cell.x+1, cell.y+1),
 			new Point(cell.x+1, cell.y),
@@ -41,15 +42,36 @@ public class ConwayRules {
 	}
 
 	public Set<Point> tick(Set<Point> world) {
+		
 		Set<Point> newWorld = Sets.newHashSet();
 		
-		for(Point cell : world) {
-			int numberOfNeighbours = countNeighbours(world, cell);
-			if(numberOfNeighbours == 2 || numberOfNeighbours == 3) {
+		for(Point cell : getAllLocationsThatCanBeAffected(world)) {
+			
+			if(world.contains(cell)
+					&& (countNeighbours(world, cell) == 2 || countNeighbours(world, cell) == 3)) {
+				newWorld.add(cell);
+			}
+			else if (countNeighbours(world, cell) == 3) {
 				newWorld.add(cell);
 			}
 		}
+		
 		return newWorld;
+	}
+
+	public Set<Point> getDeadNeighbours(Set<Point> liveCells, Point cell) {
+		
+		Set<Point> deadNeighbours = Sets.newHashSet();
+		
+		for(int i = -1; i <= 1; i++) {
+			for(int j = -1; j <= 1; j++) {
+				if(!(i == 0 && j == 0)
+						&& !liveCells.contains(new Point(i, j))) {
+					deadNeighbours.add(createRelativePoint(cell, i, j));
+				}
+			}
+		}
+		return deadNeighbours;
 	}
 
 }
